c5dfb3c70a7facca54084c55c4fb6b234676eca0
Lucas Tran <lucastran1107@gmail.com>
2024-09-05 02:21:20 +0200

    feat(frontend): :sparkles: Create navbar component

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,13 +2,16 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
-
 export default function Navbar() {
   return (
-    <nav>
-      <div className="flex flex-col">
+    <nav className="w-full border-b border-x-neutral-600 p-5">
+      <div className="flex gap-8">
         <Link href="/">
           <Button>Home</Button>
+        </Link>
+
+        <Link href="/favorites">
+          <Button>Favorites</Button>
         </Link>
       </div>
     </nav>
