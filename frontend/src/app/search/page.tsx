42f000e714c5a393d0d5274fa3c3da5e29bda437
Lucas Tran <lucastran1107@gmail.com>
2024-10-11 19:58:20 +0200

    fix(CI/CD): :green_heart: try to fix build

--- frontend/src/app/search/page.tsx
+++ frontend/src/app/search/page.tsx
@@ -1,12 +1,21 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
 import DisplayGames from "@/components/DisplayGames";
 
 const SearchResults = () => {
-    const searchParams = useSearchParams(); // Use searchParams for query parameters
-    const query = searchParams.get("query") || false; // Get the query from the URL
+    const [query, setQuery] = useState<string | false | null>(null);
+    const searchParams = useSearchParams(); // Get search params only on client-side
+
+    useEffect(() => {
+        const queryValue = searchParams.get("query") || false;
+        setQuery(queryValue);
+    }, [searchParams]);
+
+    if (query === null) {
+        return <p>Loading search results...</p>; // Can add a Suspense boundary here if needed
+    }
 
     return (
         <main className="flex min-h-screen flex-col p-24 prose prose-invert lg:prose-xl max-w-full">
@@ -14,10 +23,11 @@ const SearchResults = () => {
                 Search Results for:
                 {query ? ` ${query}` : " No query provided"}
             </h3>
-            <Suspense fallback={<p>Loading search results...</p>}>
+            {/* Suspense for search-related content */}
+            <Suspense fallback={<p>Loading games...</p>}>
                 <DisplayGames
                     cardsPerPage={12}
-                    search={encodeURIComponent(query)}
+                    search={encodeURIComponent(query || "")}
                 />
             </Suspense>
         </main>
