dde7bb5341f37e366af8efe3cfc673d5e44b92be
Lucas Tran <lucastran1107@gmail.com>
2024-10-25 20:22:52 +0200

    fix(backend): :bug: handle CORS error in springboot web server

--- backend/SpringBoot/src/main/kotlin/com/games/games/config/WebConfig.kt
+++ backend/SpringBoot/src/main/kotlin/com/games/games/config/WebConfig.kt
@@ -4,13 +4,11 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
-// Adjust the package name as needed
-
 @Configuration
 class WebConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**") // Allow all endpoints
-            .allowedOrigins("http://localhost:3000") // Allow your frontend URL
+            .allowedOrigins("http://localhost:3000", "https://spaceworld.azurewebsites.net") // Allow local and production frontend URLs
             .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS") // Allow necessary HTTP methods
             .allowCredentials(true) // Allow credentials if needed
     }
