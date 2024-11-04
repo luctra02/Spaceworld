7fe2f090e261cf672d2cd044700c22cd6068a4c6
Lucas Tran <lucastran1107@gmail.com>
2024-11-04 15:49:23 +0100

    feat(frontend): :sparkles: remove the option to log in with credentials on the server

--- frontend/src/app/api/auth/[...nextauth]/options.ts
+++ frontend/src/app/api/auth/[...nextauth]/options.ts
@@ -1,7 +1,6 @@
 import type { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import DiscordProvider from "next-auth/providers/discord";
-import CredentialsProvider from "next-auth/providers/credentials";
 
 export const options: NextAuthOptions = {
     providers: [
@@ -12,40 +11,6 @@ export const options: NextAuthOptions = {
         DiscordProvider({
             clientId: process.env.DISCORD_ID as string,
             clientSecret: process.env.DISCORD_SECRET as string,
-        }),
-        CredentialsProvider({
-            // The name to display on the sign in form (e.g. "Sign in with...")
-            name: "Credentials",
-            // `credentials` is used to generate a form on the sign in page.
-            // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-            // e.g. domain, username, password, 2FA token, etc.
-            // You can pass any HTML attribute to the <input> tag through the object.
-            credentials: {
-                username: {
-                    label: "Username",
-                    type: "text",
-                    placeholder: "jsmith",
-                },
-                password: { label: "Password", type: "password" },
-            },
-            async authorize(credentials, req) {
-                // Add logic here to look up the user from the credentials supplied
-                const user = {
-                    id: "1",
-                    name: "J Smith",
-                    email: "jsmith@example.com",
-                };
-
-                if (user) {
-                    // Any object returned will be saved in `user` property of the JWT
-                    return user;
-                } else {
-                    // If you return null then an error will be displayed advising the user to check their details.
-                    return null;
-
-                    // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-                }
-            },
         }),
     ],
 
