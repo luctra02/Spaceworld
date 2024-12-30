86cc619d19184f087815317638c21e10a9711a2c
Lucas Tran <lucastran1107@gmail.com>
2024-12-30 16:41:38 +0100

    build(backend): :rocket: add the url for springboot application in webconfig to prevent cors-error

--- backend/SpringBoot/src/main/kotlin/com/games/games/config/WebConfig.kt
+++ backend/SpringBoot/src/main/kotlin/com/games/games/config/WebConfig.kt
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class WebConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**") // Allow all endpoints
-            .allowedOrigins("http://localhost:3000", "https://spaceworld.azurewebsites.net", "https://spaceworld-springboot.azurewebsites.net", "https://spaceworld-smoky.vercel.app/", ) // Allow local and production frontend URLs
+            .allowedOrigins("http://localhost:3000", "https://spaceworld.azurewebsites.net", "https://spaceworld-springboot.azurewebsites.net", "https://spaceworld-smoky.vercel.app/", "https://spaceworld-production.up.railway.app" ) // Allow local and production frontend URLs
             .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS") // Allow necessary HTTP methods
     }
 }
